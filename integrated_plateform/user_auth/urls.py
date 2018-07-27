8d3eb3f4e62147d3fae113ef10fca73861684a51
jiangjinhu <seacracker@qq.com>
2018-07-27 21:17:16 +0800

    add login2.html

--- integrated_plateform/user_auth/urls.py
+++ integrated_plateform/user_auth/urls.py
@@ -6,5 +6,6 @@ app_name = 'user_auth'
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
+    url(r'test_', views.login2, name='login2'),
     url(r'^logout', views.logout, name='logout'),
 ]
