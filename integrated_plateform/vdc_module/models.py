723518c89ebb18777cc3ef329fd448467b0ed185
Ren Yujin <17212010068@fudan.edu.cn>
2018-07-27 14:08:48 +0800

    vdc_module user_html create update delete

--- integrated_plateform/vdc_module/models.py
+++ integrated_plateform/vdc_module/models.py
@@ -5,19 +5,20 @@ from user_auth import models as auth_models
 from django.contrib.auth.hashers import (
     check_password, is_password_usable, make_password,
 )
+from django.conf import settings
 # Create your models here.
-def print_fields(exclude,table_name):
-    obj = apps.get_model('user_auth',table_name)
-    obj_fields = obj._meta.fields
-    print_fields_list = [f for f in obj_fields if f.name not in exclude]
-    return print_fields_list
 
 def create_user_operation(request):
     name = request.POST.get('username')
     password = request.POST.get('password')
     email = request.POST.get('email')
-    role = request.POST.get('user_role')
-    user_obj = auth_models.User(name=name, password=make_password(password), email=email)
+    department = request.POST.get('department')
+    role = settings.SYSROLES['SYSUSER']
+    quota_cpu = request.POST.get('cpu_amount')
+    quota_ram = request.POST.get('memory')
+    quota_volume = request.POST.get('volume')
+    quota_instances = request.POST.get('instance_amount')
+    user_obj = auth_models.User(name=name, password=make_password(password), email=email,department=department,cpu=quota_cpu,ram=quota_ram,instances=quota_instances,volume=quota_volume,recent_use_VDC=0)
     user_obj.save()
     user_role_obj = auth_models.User_Role_VDC(user_id=user_obj.id, role_id=role)
     user_role_obj.save()
